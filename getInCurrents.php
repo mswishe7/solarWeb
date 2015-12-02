eae28d18d03976a2a23b0fd48465e2a6f16edee6
Mark Swisher <mswishe7@mix.wvu.edu>
2015-12-02 01:38:42 -0500

    Update getInCurrents.php

--- getInCurrents.php
+++ getInCurrents.php
@@ -1,8 +1,6 @@
     <?php
 $con=mysql_connect("localhost","root","solar") or die("Failed to connect with database!!!!");
 mysql_select_db("solar_tree", $con); 
-// The Chart table contains two fields: weekly_task and percentage
-// This example will display a pie chart. If you need other charts such as a Bar chart, you will need to modify the code a little to make it work with bar chart and other charts
 $sth = mysql_query("SELECT * FROM InputCurrent");
 
 
@@ -13,8 +11,6 @@ $flag = true;
 $table = array();
 $table['cols'] = array(
 
-    // Labels for your chart, these represent the column titles
-    // Note that one column is in "string" format and another one is in "number" format as pie chart only required "numbers" for calculating percentage and string will be used for column title
     array('label' => 'Position', 'type' => 'number'),       
     array('label' => 'Input Current', 'type' => 'number'),
 
@@ -28,7 +24,6 @@ $pos = 1;
 if($nr <= 50) {
        while($r = mysql_fetch_assoc($sth)) {
        		$temp = array();
-       		// the following line will be used to slice the Pie chart
     		$temp[] = array('v' =>  $pos);
     		$temp[] = array('v' => (float) $r['InputCurrent']);
 		$rows[] = array('c' => $temp);
@@ -56,13 +51,3 @@ $table['rows'] = $rows;
 $jsonTable = json_encode($table);
 echo $jsonTable;
 ?>
-
-
-
-
-
-
-
-
-
-
