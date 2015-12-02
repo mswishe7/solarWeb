eb47c7897e35dbaa87190b90a890667e496448b9
Mark Swisher <mswishe7@mix.wvu.edu>
2015-12-02 01:39:58 -0500

    Update getTemps.php

--- getTemps.php
+++ getTemps.php
@@ -1,8 +1,6 @@
     <?php
 $con=mysql_connect("localhost","root","solar") or die("Failed to connect with database!!!!");
 mysql_select_db("solar_tree", $con); 
-// The Chart table contains two fields: weekly_task and percentage
-// This example will display a pie chart. If you need other charts such as a Bar chart, you will need to modify the code a little to make it work with bar chart and other charts
 $sth = mysql_query("SELECT * FROM stats");
 
 $nr=mysql_num_rows($sth);
@@ -11,9 +9,6 @@ $rows = array();
 $flag = true;
 $table = array();
 $table['cols'] = array(
-
-    // Labels for your chart, these represent the column titles
-    // Note that one column is in "string" format and another one is in "number" format as pie chart only required "numbers" for calculating percentage and string will be used for column title
     array('label' => 'Position', 'type' => 'number'),       
     array('label' => 'Heatsink Temperature', 'type' => 'number'),
 
@@ -27,7 +22,7 @@ $pos = 1;
 if($nr <= 50) {
        while($r = mysql_fetch_assoc($sth)) {
        		$temp = array();
-       		// the following line will be used to slice the Pie chart
+
     		$temp[] = array('v' =>  $pos);
     		$temp[] = array('v' => (float) $r['HeatsinkTemp']); 
 		$rows[] = array('c' => $temp);
@@ -40,8 +35,7 @@ else {
      	      if($pos > $a) {    
 
 	      	      $temp = array();
-    		      // the following line will be used to slice the Pie chart
-    		      $temp[] = array('v' =>  $pos);
+   		      $temp[] = array('v' =>  $pos);
     		      $temp[] = array('v' => (float) $r['HeatsinkTemp']);    
 		      $rows[] = array('c' => $temp);
 		      }
@@ -53,15 +47,4 @@ $table['rows'] = $rows;
 $jsonTable = json_encode($table);
 echo $jsonTable;
 ?>
-
-
-
-
-
-
-
-
-
-
-
 
